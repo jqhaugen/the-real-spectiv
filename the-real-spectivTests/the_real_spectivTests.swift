3511d3180237fd47061f13c4547de74c879d94ca
Jason Haugen <jqhaugen2014@gmail.com>
2016-11-17 00:27:23 -0600

    Initial Commit

---
+++ the-real-spectivTests/the_real_spectivTests.swift
@@ -0,0 +1,36 @@
+//
+//  the_real_spectivTests.swift
+//  the-real-spectivTests
+//
+//  Created by Jason Haugen on 11/17/16.
+//  Copyright © 2016 Jason Haugen. All rights reserved.
+//
+
+import XCTest
+@testable import the_real_spectiv
+
+class the_real_spectivTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
