0baa00c37c570079370b172d4873bea6443ded9c
Jason Haugen <jqhaugen2014@gmail.com>
2016-11-17 00:50:00 -0600

    Initial commit - fixed everything I did before for 3.0 - basic UI Skeleton mostly complete

---
+++ the-real-spectiv/PictureCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PictureCollectionViewCell.swift
+//  spectiv1
+//
+//  Created by Jason Haugen on 11/10/16.
+//  Copyright © 2016 Jason Haugen. All rights reserved.
+//
+
+import UIKit
+
+class PictureCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var theImage: UIImageView!
+    
+    
+}
---
+++ the-real-spectiv/SearchViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  spectiv1
+//
+//  Created by Jason Haugen on 11/10/16.
+//  Copyright © 2016 Jason Haugen. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
+        
+    @IBOutlet weak var theCollection: UICollectionView!
+    
+    var selection: Int = 0;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        theCollection.backgroundColor = UIColor.white
+        theCollection.reloadData()
+        
+        theCollection.dataSource = self
+        theCollection.delegate = self
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // ======================================
+    // collection view functions
+    // ======================================
+    
+    // required function to figure out how many cells in the collection view
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15;
+    }
+    
+    // required function to figure out what to put in each cell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! PictureCollectionViewCell
+        
+        // dynamically change the image of a cell
+        //cell.theImage.image = ?????
+        
+        return cell
+    }
+    
+    // set the var selection to whatever the user tapped on to know what index in myArray to pass along to the detail view
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selection = indexPath.item
+    }
+    
+    
+    
+}
+
